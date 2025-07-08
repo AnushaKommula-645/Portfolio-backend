76d3e8dbe86425cb66b7ccb68f3fd049d97a442e
anushakommula645 <anushakommula645@gmail.com>
2025-07-09 01:02:57 +0530

    fix: use process.env.PORT for Render compatibility

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import nodemailer from 'nodemailer';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
 
 app.use(cors());
 app.use(express.json());
